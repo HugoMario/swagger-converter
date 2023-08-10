ff09daf3242f6a0203d3d5837bb81ba99fb9c17d
Hugo Mercado <hugo.mario.mercado@gmail.com>
2023-08-10 00:45:09 -0500

    refactored bump version script

--- CI/bump-version.sh
+++ CI/bump-version.sh
@@ -1,54 +1,44 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $DIR
+#function bump {
+#  local mode="$1"
+#  local old="$2"
+#  local parts=( ${old//./ } )
+#  case "$1" in
+#    major)
+#      local bv=$((parts[0] + 1))
+#      NEW_VERSION="${bv}.0.0"
+#      ;;
+#    minor)
+#      local bv=$((parts[1] + 1))
+#      NEW_VERSION="${parts[0]}.${bv}.0"
+#      ;;
+#    patch)
+#      local bv=$((parts[2] + 1))
+#      NEW_VERSION="${parts[0]}.${parts[1]}.${bv}"
+#      ;;
+#    esac
+#}
 
-function bump {
-  local mode="$1"
-  local old="$2"
-  local parts=( ${old//./ } )
-  case "$1" in
-    major)
-      local bv=$((parts[0] + 1))
-      NEW_VERSION="${bv}.0.0"
-      ;;
-    minor)
-      local bv=$((parts[1] + 1))
-      NEW_VERSION="${parts[0]}.${bv}.0"
-      ;;
-    patch)
-      local bv=$((parts[2] + 1))
-      NEW_VERSION="${parts[0]}.${parts[1]}.${bv}"
-      ;;
-    esac
-}
+CURRENT_VERSION="$(./CI/version.sh)"
 
-OLD_VERSION="$($DIR/version.sh)"
+echo "old version is: " $CURRENT_VERSION
 
-echo "old version is: " $OLD_VERSION
+CURRENT_VERSION=${CURRENT_VERSION/-SNAPSHOT/}
+echo "old version AGAIN is: " $CURRENT_VERSION
 
-OLD_VERSION=${OLD_VERSION/-SNAPSHOT/}
-echo "old version AGAIN is: " $OLD_VERSION
+#NEW_VERSION=${CURRENT_VERSION/-SNAPSHOT/}
+#
+#echo "new version is: " $NEW_VERSION " SNAPSHOT"
+PARTS=( ${CURRENT_VERSION//./ })
+BUMPED_VERSION=$((PARTS[2] + 1))
+NEW_VERSION="${PARTS[0]}.${PARTS[1]}.${BUMPED_VERSION}-SNAPSHOT"
 
+#bump $BUMP_MODE $CURRENT_VERSION
 
-NEW_VERSION=${OLD_VERSION/-SNAPSHOT/}
-
-echo "new version is: " $NEW_VERSION
-
-BUMP_MODE="patch"
-if git log -1 | grep -q "#major"; then
-  BUMP_MODE="major"
-elif git log -1 | grep -q "#minor"; then
-  BUMP_MODE="minor"
-elif git log -1 | grep -q "#patch"; then
-  BUMP_MODE="patch"
-fi
-
-echo $BUMP_MODE "version bump detected"
-bump $BUMP_MODE $OLD_VERSION
-echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
+echo "pom.xml will be bumped from ${CURRENT_VERSION} to ${NEW_VERSION}"
 mvn -q versions:set -DnewVersion="${NEW_VERSION}"
 #  git add $POMPATH/pom.xml
 #  REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
-#  git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
+#  git commit -m "Bump pom.xml from $CURRENT_VERSION to $NEW_VERSION"
 #  git tag $NEW_VERSION
 #  git push $REPO --follow-tags
 #  git push $REPO --tags
