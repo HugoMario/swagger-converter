fdc177c5dd54d050db81505ea109248fe359873c
Hugo Mercado <hugo.mario.mercado@gmail.com>
2023-08-11 00:07:55 -0500

    kkk

--- CI/bump-version.sh
+++ CI/bump-version.sh
@@ -4,16 +4,7 @@ CURRENT_VERSION="$(./CI/version.sh)"
 
 echo "old version is: " ${CURRENT_VERSION}
 
-delimiter='.'
-s=$CURRENT_VERSION$delimiter
-PARTS=();
-while [[ $s ]]; do
-    PARTS+=("${s%%"$delimiter"*}");
-    s=${s#*"$delimiter"};
-done;
-
-BUMPED_VERSION=$((PARTS[2] + 1))
-NEW_VERSION="${PARTS[0]}.${PARTS[1]}.${BUMPED_VERSION}-SNAPSHOT"
+NEW_VERSION=`python ./CI/nextVersion.py`
 
 echo "pom.xml will be bumped from ${CURRENT_VERSION} to ${NEW_VERSION}"
 mvn -q versions:set -DnewVersion="${NEW_VERSION}"
---
+++ CI/nextVersion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# main
+def main(tag):
+    print "1.0.5-SNAPSHOT"
+
+# here start main
+main(sys.argv[1])
