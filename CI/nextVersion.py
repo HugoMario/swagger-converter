70fa4428d9d787834ad892a0dfe6842e0dad0568
Hugo Mercado <hugo.mario.mercado@gmail.com>
2023-08-11 17:29:36 -0500

    qqq

--- CI/nextVersion.py
+++ CI/nextVersion.py
@@ -4,14 +4,11 @@ import sys
 
 # main
 def main(tag):
-    isSnapshot = False
     if "SNAPSHOT" in tag:
         tag = tag.replace("-SNAPSHOT", "")
-        isSnapshot = True
     tagParts = tag.split(".")
     bumped = tagParts[0] + "." + tagParts[1] + "." + str(int(tagParts[2]) + 1)
-    if isSnapshot:
-        bumped += "-SNAPSHOT"
+    bumped += "-SNAPSHOT"
     print bumped
 # here start main
 main(sys.argv[1])
